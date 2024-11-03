43dfb46204bc1d209abc0aec3bb88af6bcb767a9
Anitha-K2006 <anitha3112kumar@gmail.com>
2024-11-03 14:15:01 +0530

    Update Abundant Number.c

--- Abundant Number.c
+++ Abundant Number.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main()
+{
+  int n;
+  int sum=0;
+  scanf("%d",&n);
+  for(int i=2;i<=n/2;i++)
+  {
+    if(n%i==0)
+    {
+      sum=sum+i;
+    }
+  }
+  if(sum>n)
+  {
+    printf("True");
+  }
+  else
+  {
+    printf("False");
+  }
+  return 0;
+}
