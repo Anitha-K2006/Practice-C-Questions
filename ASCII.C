06492fc62605c1c9e9384093e2781edb9f267cef
Anitha-K2006 <anitha3112kumar@gmail.com>
2024-12-14 10:06:35 +0530

    Update ASCII.C

--- ASCII.C
+++ ASCII.C
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include<string.h>
 int main() {
-   char lett[25];
+   char letter[25];
    scanf("%s",lett);
    int l=0;
    l=strlen(lett);
-   int high=0,ascii=0,temp;
+   int char=0,ascii=0,t;
    for(int i=0;i<l-1;i++)
    {
-       temp=1;
-       if(lett[i]!='@')
+       t=1;
+       if(letter[i]!='%')
        {
        for(int j=i+1;j<l;j++)
        {
-           if(lett[i]==lett[j])
+           if(letter[i]==letter[j])
            {
-               lett[j]='@';
-               temp++;
+               letter[j]='@';
+               t++;
            }
        }
-       if(((temp==1)||(temp%2==1))&&(temp>high))
+       if(((t==1)||(t%2==1))&&(t>char))
        {
-           high=temp;
-           ascii=(int)lett[i];
+           char=t;
+           ascii=(int)letter[i];
        }
        }
  }
